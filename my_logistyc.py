07b97b7fcc3528ea60faa9edddee62b1ccdb3989
Sergey-2021 <tatarinov.serzhick@yandex.ru>
2022-02-27 07:24:35 +0300

    v.2.0

--- my_logistyc.py
+++ my_logistyc.py
@@ -1,67 +1,46 @@
 import os.path
-"""
-Метод is_there_file проверяет существует ли файл
-
-"""
+import time
 
 
 def is_there_file(name_id):
+    """Метод is_there_file проверяет существует ли файл"""
     element = os.path.isfile(f'{name_id}.csv')
     return element
 
 
-"""
-Метод add_log создает и дописывает файл по id
-В файл записывается:  остаток конфет/ общее кол-во конфет/ максимальное кол-во за ход/ сделал игрок ход или нет
 
-"""
+def add_log(name, keep, my_list):
+    """Метод add_log создает и дописывает файл по id
 
-def add_log(name, keep, candies = 300, max_bet = 14):
-    key = 0
+    Принимает id, остаток конфет, список с условиями игры [кол-во конфет, сколько конфет можно взять за ход, чей ход]"""
     if not is_there_file(name):
         with open(f'{name}.csv', 'a+', encoding='utf8') as data:
-            data.write(f'{keep}, {candies}, {max_bet}, {key}; ')
+            data.write(f'{time.strftime("%Y-%m-%d,%H:%M:%S")},{keep},{my_list};\n')
     else:
-        your_last_move = last_move(name)
-        if your_last_move == '0':
-            key = 1
-            if keep == 0:
-                with open(f'{name}.csv', 'a+', encoding='utf8') as data1:
-                    data1.write(f'{keep}, {candies}, {max_bet}, {key}\n')
-            else:
-                with open(f'{name}.csv', 'a+', encoding='utf8') as data2:
-                    data2.write(f'{keep}, {candies}, {max_bet}, {key}; ')
+        if keep == 0:
+            with open(f'{name}.csv', 'a+', encoding='utf8') as data1:
+                data1.write(f'{time.strftime("%Y-%m-%d,%H:%M:%S")},{keep},{my_list};\n')
         else:
-            key = 0
-            if keep == 0:
-                with open(f'{name}.csv', 'a+', encoding='utf8') as data1:
-                    data1.write(f'{keep}, {candies}, {max_bet}, {key}\n')
-            else:
-                with open(f'{name}.csv', 'a+', encoding='utf8') as data2:
-                    data2.write(f'{keep}, {candies}, {max_bet}, {key}; ')
+            with open(f'{name}.csv', 'a+', encoding='utf8') as data2:
+                data2.write(f'{time.strftime("%Y-%m-%d,%H:%M:%S")},{keep},{my_list};')
 
 
-"""
-Метод last_move
-
-"""
 
 def last_move(name_id):
-    with open(f'{name_id}.csv', 'r', encoding='utf8') as my_file:
-        f = my_file.read()
-        f = f.split('\n')
-        if f[-1] == '':
-            t = f[-2]
-            return t[-3]
-        else:
-            t = f[-1]
-            return t[-3]
+    """Метод last_move возвращает результаты последнего хода(последняя запись в файле)
 
-
-add_log('S2022', 4)
-last_move('S2022')
-
-
-
-
-
+       Принимает id  """
+    if not is_there_file(name_id):
+        return 'Файла нет'
+    else:
+        with open(f'{name_id}.csv', 'r', encoding='utf8') as my_file:
+            f = my_file.read()
+            f = f.split('\n')
+            if f[-1] == '':
+                f.remove(f[-1])
+            last_game = f[-1]
+            last_game = last_game.split(';')
+            if last_game[-1] == '':
+                return last_game[-2]
+            else:
+                return last_game[-1]
